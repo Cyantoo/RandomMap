5de2a2325acf58f4065ffd13d8e2e8c569c430c4
Antoine Colin <antoine.colin@gemelligames.com>
2024-03-28 16:08:21 +0100

    Add parameters to generator function

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 from itertools import chain, zip_longest
 
 
-def get_random_map(row_count, col_count):
+def get_random_map(row_count, col_count, obstacle_density, obscuring_proportion):
     return [
-        [("." if random.randint(0, 5) < 4 else ("o" if random.randint(0, 1) == 0 else "x")) for _ in range(col_count)]
+        [("." if random.random() > obstacle_density else ("o" if random.random() < obscuring_proportion else "x")) for _ in range(col_count)]
         for _ in range(row_count)]
 
 
@@ -12,5 +12,5 @@ def pretty_matrix(matrix):
     return "\n".join("  ".join(row) for row in matrix)
 
 for i in range(10):
-    print(pretty_matrix(get_random_map(10, 10)))
+    print(pretty_matrix(get_random_map(10, 10, 1./3, 1./2)))
     print()
